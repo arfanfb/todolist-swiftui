3b4aa973aef4acca5895a0d85b96f791911770f8
arfan.tkp103@digital.telkom.co.id <arfan.febriyanto@digital.telkom.co.id>
2025-02-19 14:48:13 +0700

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Arfan on 19/02/25.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
